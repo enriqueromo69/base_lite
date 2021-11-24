efb27cced9e483f0de78c09bbb561806a59e9e19
Victo <enriqueromo69@hotmail.com>
2021-11-24 01:23:55 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .styles([],'public/css/admin.css')
+    .scripts([],'public/js/admin.js');
---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+
+    
+    <h1>Hola
+    <span class="ion-android-arrow-dropright-circle"></span>
+    </h1>
+
+    <h1>h
+        <span class="step size-96">
+        <i class="icon ion-android-bar"></i>
+        </span>
+        
+    </h1>
+    </div>
+</div>
+@endsection
